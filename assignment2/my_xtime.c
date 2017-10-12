51671d9ca91a4d836af76ed2dcc74001af77d01e
csvb <csvb@CS350-eli24.csvb.local>
2017-10-12 12:01:07 -0400

    Added code

--- assignment2/my_xtime.c
+++ assignment2/my_xtime.c
@@ -1,1 +1,19 @@
-/* my_xtime system call */
+/* my_xtime system call
+ * Name: Eric W. Li
+ * CS 350: Operating Systems
+ */
+
+#include <linux/time.h>
+
+asmlinkage int my_xtime(struct timespec *current_time) {
+	/* Check that memory pointed to by current_time is writable */
+	if (!access_ok(VERIFY_WRITE, current_time, sizeof(struct timespec)) {
+		return -EFAULT;
+	}
+
+	struct timespec k_time = current_kernel_time();
+	copy_to_user(current_time, k_time, sizeof(struct timespec));
+	printk(KERN_ALERT "current kernel time = %d\n", k_time.tv_nsec);
+
+	return 0;
+}
